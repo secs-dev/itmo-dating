ea02a4f16f082497378fd78725b69065534f5e85
Victor Smirnov <53015676+vityaman@users.noreply.github.com>
2024-09-19 16:03:09 +0300

    #1 Host OpenAPI specification web page (#4)

---
+++ people/app/src/main/java/ru/ifmo/se/dating/people/Main.java
@@ -0,0 +1,11 @@
+package ru.ifmo.se.dating.people;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
