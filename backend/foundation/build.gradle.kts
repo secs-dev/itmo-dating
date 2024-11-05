a446969bfb1b96665b2c52003feada6ca5b5926a
vityaman <vityaman.dev@yandex.ru>
2024-11-05 11:48:59 +0300

    #45 Move common dependencies to foundation

--- backend/foundation/build.gradle.kts
+++ backend/foundation/build.gradle.kts
@@ -1,3 +1,23 @@
 plugins {
     id("buildlogic.kotlin-library-conventions")
 }
+
+dependencies {
+    api(libs.io.swagger.core.v3.swagger.annotations)
+    api(libs.io.swagger.core.v3.swagger.models)
+    api(libs.org.openapitools.jackson.databind.nullable)
+    api(libs.org.springdoc.springdoc.openapi.starter.webmvc.ui)
+
+    api(libs.org.springframework.spring.web)
+    api(libs.org.springframework.spring.context)
+
+    api(libs.org.springframework.boot.spring.boot)
+    api(libs.org.springframework.boot.spring.boot.starter.web)
+
+    api(libs.org.springframework.boot.spring.boot.starter.jdbc)
+    api(libs.org.liquibase.liquibase.core)
+    api(libs.org.postgresql.postgresql)
+
+    api(libs.com.fasterxml.jackson.core.jackson.databind)
+    api(libs.jakarta.validation.jakarta.validation.api)
+}
