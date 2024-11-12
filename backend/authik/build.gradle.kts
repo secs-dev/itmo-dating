6e823b081dd0ab4d84b1f9d6bdfd833991280aea
vityaman <vityaman.dev@yandex.ru>
2024-11-12 08:57:52 +0300

    #54 Initialize an empty authik service

---
+++ backend/authik/build.gradle.kts
@@ -0,0 +1,15 @@
+@file:Suppress("UNCHECKED_CAST")
+
+plugins {
+    id("buildlogic.foundation-conventions")
+}
+
+dependencies {
+    implementation(project(":foundation"))
+    testImplementation(project(":foundation-test"))
+}
+
+val serviceName = "authik"
+
+(extra["generateOAPIServer"] as (String) -> Unit)(serviceName)
+(extra["generateJOOQ"] as (String) -> Unit)(serviceName)
