5fe7f4be66cbb850773ab35d79f2a80b085b3ea3
vityaman <vityaman.dev@yandex.ru>
2024-11-12 16:57:02 +0300

    #54 Fix build

--- backend/matchmaker/src/test/kotlin/ru/ifmo/se/dating/matchmaker/MatchmakerTestSuite.kt
+++ backend/matchmaker/src/test/kotlin/ru/ifmo/se/dating/matchmaker/MatchmakerTestSuite.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringRunner
+import ru.ifmo.se.dating.Application
 
 @RunWith(SpringRunner::class)
 @ActiveProfiles(profiles = ["test"])
