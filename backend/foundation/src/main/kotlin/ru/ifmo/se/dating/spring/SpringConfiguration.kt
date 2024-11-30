e8bbba185d65dec3f6f5c16ce1a10c17cd680d45
vityaman <vityaman.dev@yandex.ru>
2024-11-30 09:22:51 +0300

    #45 Enable scheduling

---
+++ backend/foundation/src/main/kotlin/ru/ifmo/se/dating/spring/SpringConfiguration.kt
@@ -0,0 +1,8 @@
+package ru.ifmo.se.dating.spring
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Configuration
+@EnableScheduling
+class SpringConfiguration
