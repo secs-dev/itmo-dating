3fa969fba2c0c152b5c52b8b2633636cec5df27d
vityaman <vityaman.dev@yandex.ru>
2025-01-06 16:11:16 +0300

    #61 Configure redirection
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

--- backend/gateway/build.gradle.kts
+++ backend/gateway/build.gradle.kts
@@ -6,6 +6,8 @@ dependencies {
     implementation(libs.org.springframework.boot.spring.boot)
     implementation(libs.org.springframework.cloud.spring.cloud.starter.gateway)
 
+    implementation(libs.org.springdoc.springdoc.openapi.starter.webflux.ui)
+
     testImplementation(libs.org.springframework.boot.spring.boot.starter.test)
     testImplementation(libs.junit.junit)
 }
