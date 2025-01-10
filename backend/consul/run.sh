36b2b91c4b0370c82056c0efdc3756e61dac15a3
vityaman <vityaman.dev@yandex.ru>
2025-01-10 14:28:33 +0300

    #68 Fix consul image

---
+++ backend/consul/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+consul agent -config-file=/consul/config/consul.hcl -bootstrap-expect=1 \
+  | grep -v "This request used the token query parameter which is deprecated and will be removed"
