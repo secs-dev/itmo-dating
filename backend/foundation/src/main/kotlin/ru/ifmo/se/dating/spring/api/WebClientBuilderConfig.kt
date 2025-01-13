4d68341962e679ed52867c3340947e44fd5e3ccd
vityaman <vityaman.dev@yandex.ru>
2025-01-13 15:07:30 +0300

    #68 Fix testing load balancing
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

--- backend/foundation/src/main/kotlin/ru/ifmo/se/dating/spring/api/WebClientBuilderConfig.kt
+++ backend/foundation/src/main/kotlin/ru/ifmo/se/dating/spring/api/WebClientBuilderConfig.kt
@@ -1,8 +1,9 @@
 package ru.ifmo.se.dating.spring.api
 
 import io.netty.handler.ssl.SslContext
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Primary
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
@@ -14,11 +15,17 @@ class WebClientBuilderConfig(
 ) {
     @Bean
     @LoadBalanced
-    fun webClientBuilder() = WebClient.builder()
-        .clientConnector(
-            ReactorClientHttpConnector(
-                HttpClient.create()
-                    .secure { sslContextSpec -> sslContextSpec.sslContext(ssl) }
-            ),
+    fun webClientBuilderLoadBalanced(): WebClient.Builder = WebClient.builder()
+        .clientConnector(connector())
+
+    @Bean
+    @Primary
+    fun webClientBuilder(): WebClient.Builder = WebClient.builder()
+        .clientConnector(connector())
+
+    private fun connector() =
+        ReactorClientHttpConnector(
+            HttpClient.create()
+                .secure { sslContextSpec -> sslContextSpec.sslContext(ssl) }
         )
 }
--- backend/people/src/main/kotlin/ru/ifmo/se/dating/people/external/MatchmakerApi.kt
+++ backend/people/src/main/kotlin/ru/ifmo/se/dating/people/external/MatchmakerApi.kt
@@ -2,6 +2,7 @@ package ru.ifmo.se.dating.people.external
 
 import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import ru.ifmo.se.dating.matchmaker.client.generated.apis.PeopleApi
@@ -12,6 +13,7 @@ class MatchmakerApi(
     @Value("\${service.matchmaker.url}")
     private val url: String,
 
+    @LoadBalanced
     webClient: WebClient.Builder,
 ) {
     private val people = PeopleApi(webClient.baseUrl(url).build())
