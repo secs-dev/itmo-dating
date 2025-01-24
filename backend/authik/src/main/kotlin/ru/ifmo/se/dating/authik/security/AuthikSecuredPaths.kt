d912a8dc8cefcc6171fdc89e77f5d9babf3e6eb6
vityaman <vityaman.dev@yandex.ru>
2025-01-24 15:26:56 +0300

    #88 Get Spring Boot metrics

--- backend/authik/src/main/kotlin/ru/ifmo/se/dating/authik/security/AuthikSecuredPaths.kt
+++ backend/authik/src/main/kotlin/ru/ifmo/se/dating/authik/security/AuthikSecuredPaths.kt
@@ -1,11 +1,19 @@
 package ru.ifmo.se.dating.authik.security
 
+import org.springframework.http.HttpMethod
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher
 import org.springframework.stereotype.Component
-import ru.ifmo.se.dating.spring.security.auth.SpringSecuredPaths
+import ru.ifmo.se.dating.spring.security.auth.And
+import ru.ifmo.se.dating.spring.security.auth.Not
 import ru.ifmo.se.dating.spring.security.auth.Path
+import ru.ifmo.se.dating.spring.security.auth.SpringSecuredPaths
 
 @Component
 class AuthikSecuredPaths : SpringSecuredPaths {
-    override val matcher: ServerWebExchangeMatcher = Path("")
+    override val matcher: ServerWebExchangeMatcher = And(
+        Path("/api/**"),
+        Not(Path("/api/auth/telegram/web-app", HttpMethod.GET)),
+        Not(Path("/api/monitoring/healthcheck", HttpMethod.PUT)),
+        Not(Path("/actuator/**", HttpMethod.GET)),
+    )
 }
--- backend/starter-service-discovery/build.gradle.kts
+++ backend/starter-service-discovery/build.gradle.kts
@@ -7,6 +7,7 @@ dependencies {
     api(project(":starter-tls"))
 
     api(libs.org.springframework.boot.spring.boot.starter.actuator)
+    api(libs.io.micrometer.micrometer.registry.prometheus)
     api(libs.org.springframework.cloud.spring.cloud.starter.consul.discovery)
     api(libs.org.springframework.cloud.spring.cloud.starter.loadbalancer)
 }
